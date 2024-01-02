52edacd13dd2296afbd877e6024094acfd40eae3
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-02 16:00:58 +0900

    from initial commit to setup database

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(length=200)
+    private String subject;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        System.out.println("-".repeat(100));
+        return "sbb index page";
+    }
+}
