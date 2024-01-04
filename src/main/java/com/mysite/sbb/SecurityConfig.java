f06a189dd387da08e1d38f643cccf8da93c20583
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-04 15:23:30 +0900

    - 로그아웃 구현하기 + SecurityConfig

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -31,6 +31,10 @@ public class SecurityConfig {
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
                 .formLogin((formLogin) -> formLogin.loginPage("/user/login").defaultSuccessUrl("/"))
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
         ;
         return http.build();
     }
