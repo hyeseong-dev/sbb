18b4328d7abb413c6a79259fac9993097589a604
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-05 15:53:50 +0900

    feat : @hyeseong-dev 검색 기능 구현 - @Query 애너테이션 사용하기

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -24,12 +24,19 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+//    public Page<Question> getList(int page, String kw) {
+//        List<Sort.Order> sorts = new ArrayList<>();
+//        sorts.add(Sort.Order.desc("createDate"));
+//        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+//        Specification<Question> spec = search(kw);
+//        return this.questionRepository.findAll(spec, pageable);
+//    }
+
     public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        Specification<Question> spec = search(kw);
-        return this.questionRepository.findAll(spec, pageable);
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
 
     public Question getQuestion(Integer id) {
